e81cb1cf7f365908e1023c57d7e4a80d4515b9f0
yooveloper <yooveloper@gmail.com>
2022-11-26 23:03:56 +0900

    ✅ image 컴포넌트 구현

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,33 +1,17 @@
-import { BaseComponent } from '../../component';
+import { BaseComponent } from './../../component.js';
 
-/**
- * 이미지 컴포넌트는 생성자함수에서 title과 url을 전달받아서
- * 내부적으로 template 태그를 사용해서 image 요소를 동적으로 만든다.
- */
 export class ImageComponent extends BaseComponent<HTMLElement> {
-  private element: HTMLElement;
   constructor(title: string, url: string) {
-    super(`
-    <section class="image">
-      <div class="image__holder">
-        <img src="" alt="" class="image__thumbnail" />
-      </div>
-      <p class="image__title"></p>
-    </section>`);
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail"></div>
+            <h2 class="image__title"></h2>
+          </section>`);
 
-    // title과 url을 사용자에게 직접 입력받은 값으로 innerHTML에 사용하는건 안전하지 않기 때문에
-    // 이런식으로 직접 따로 설정해준다.
     const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
     imageElement.src = url;
     imageElement.alt = title;
 
     const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  // attachTo 는 외부에서 사용할 수 있는 api
-  // 추가하고 싶은 element와 position을 인자로 받는다.
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
