38cfd4ce6b0926466be8be8daedc894057bdc043
yooveloper <yooveloper@gmail.com>
2022-11-26 23:04:15 +0900

    ✅ page 컴포넌트를 감싸는 PageItem 컴포넌트 구현

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,30 @@
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { Component } from './components/component.js';
 
 class App {
-  private readonly page: PageComponent;
-  // 생성자함수에서 해당 element를 받아서 page 컴포넌트를 생성하고 attachTo 메서드로 해당 element를 추가해준다.
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    this.page.addChild(note);
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    this.page.addChild(todo);
+
+    // https://www.youtube.com/watch?v=K3-jG52XwuQ
+    // https://youtu.be/K3-jG52XwuQ
+    const video = new VideoComponent('Video Title', 'https://youtu.be/K3-jG52XwuQ');
+    this.page.addChild(video);
   }
 }
 
-// 프로그램이 실행되면 App 클래스의 인스턴스를 생성하고 인자로 document class를 가진 element를 넘긴다.
 new App(document.querySelector('.document')! as HTMLElement);
